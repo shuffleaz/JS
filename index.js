bc4184543b8dd82c633680c25ca745bfa0c3b8e1
Agshin Rajabov <akorecebov@gmail.com>
2019-07-02 13:22:24 +0400

    Yeni Ders

--- index.js
+++ index.js
@@ -1,26 +1,88 @@
-var sekiller = ['nature.jpg','dog.jpg', 'cat.jpg','ferret.jpg'];
-// console.log('Şəkil ' + sekiller[0]);
-// console.log('Şəkil ' + sekiller[1]);
-// console.log('Şəkil ' + sekiller[2]);
-// console.log('Şəkil ' + sekiller[3]);
+// var number = 5;
+// var number2 = 10;
 
-// for(var i = 0; i < sekiller.length; i++) {
-//     console.log('Şəkil ' + sekiller[i]);
+// if(number < number2) {
+//     console.log('Dəyər: ' + number);
+//     number++;
+//     console.log('Dəyər: ' + number);
 // }
 
-// var text = 0;
-// while(text < 5) {
-//     console.log('Salam')
-//     text++;
+// while(number < number2) {
+//     number++; // 10
+//     console.log(number); //5dəfə konsol log işlədi
 // }
 
-var number = 0;
+/* Numbers - Rəqəmlər */
+// var name = "Agshin";
+// console.log('Mənim adım ' + name);
 
-do {
-    console.log('My Number ' + sekiller[number]);
-    number++;
-} 
+var numberOne = "19";
+var numberTwo = 90;
+var netice = numberOne + numberTwo;
+var neticeninTipi = typeof(netice); // Nəticənin tipini öyrənmək (string, number, boolean)
 
-while (number < sekiller.length)
+var qaliq = 9;
+var round = Math.round(qaliq); // Qalıq hansına yaxındırsa ona yuvarla
+var floor = Math.floor(qaliq); // Alta yuvarla
+var ceil = Math.ceil(qaliq); // Üstə yuvarla
+var max = Math.max(19,80,5,23); // Maksimum dəyəri seç
+var min = Math.min(19,80,5,23); // Minimum dəyəri seç
+var PI = Math.PI; // Pi Sayısı
 
+/* NaN - Not A Number - Bu Rəqəm Deyil */
+
+var deyer = 1990;
+
+/* NaN - negativ elementdi. */
+/* ! - pozitivliyi negativ eləmək üçün istifadə olunur */
+
+// if(isNaN(deyer)) {
+//     console.log(deyer +' rəqəm deyil!')
+// } else {
+//     console.log(deyer +' rəqəmdir!')
+// }
+
+
+// let status = false;
+// if(status === false) {
+//     console.log(status);
+//     console.log('Bərabərdir!')
+// } else {
+//     console.log(status);
+//     console.log('Bərabər deyil!')
+// }
+
+// let status2 = false;
+// if(!status2) {
+//     console.log(status2);
+//     console.log('Bərabərdir!')
+// } else {
+//     console.log(status2);
+//     console.log('Bərabər deyil!')
+// }
+
+
+/* String - Mətn */
+var text = "Salam, 'Azərbaycan'!" //Alternativ 1
+var text = "Salam, \"Azərbaycan\"!" //Alternativ 2
+var olke = "Azərbaycan!";
+var salamOlkeText = "Salam " + olke;
+var yazininUzunlugu = salamOlkeText.length;
+
+var uzunText = "Salam, Azərbaycan Respublikası Vətəndaşları və digərləri!"
+
+var uzunTextYoxla = uzunText.indexOf('Salam1');
+
+var uppercase = uzunText.toUpperCase();
+var lowercase = uzunText.toLowerCase();
+
+
+
+console.log(lowercase);
+
+// if(uzunTextYoxla === -1) {
+//     console.log('Xeyir mövcud deyil!')
+// } else {
+//     console.log('Bəli bu yazı mətndə mövcuddur.')
+// }
 
