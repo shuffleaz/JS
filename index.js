b072ae4108f11da4adccdaf339f606e567f93360
Agshin Rajabov <akorecebov@gmail.com>
2019-07-09 11:25:47 +0400

    New Update

--- index.js
+++ index.js
@@ -1,16 +1,84 @@
-function yoxla(day) {
-    if(day == 20) {
-        return console.log('Doğum günün Mübarək!')
+// function hesablama(a, b = 1) {
+//     return a * b;
+// }
+
+// function status(userStatus = false) {
+//     if(userStatus == true) {
+//         return 'Online!'
+//     } else {
+//         return 'Offline';
+//     }
+// }
+
+// var istifadeciVeziyyeti = status();
+
+// document.write(istifadeciVeziyyeti);
+// // Offline // false
+
+
+// console.log(hesablama(6,5));
+
+// Mercedes
+// E220
+// CLA10
+// GL120
+// 4 teker
+// Benzin, Diesel
+// 
+
+// var car = new Object();
+// car.marka = "Merecedes";
+// car.model = 'CLA120';
+// car.petron = 'Benzin';
+// car.wheels = 4;
+// car.speed = 60;
+// car.drive = function() {
+//     console.log(car.marka + ' markalı və ' + car.model + ' model maşın ' + car.speed + ' ilə gedir');
+// }
+// car.temir = function(status = false) {
+//     if(status == true) {
+//         console.log('Maşın Təmirə getdi.');
+//     } else {
+//         console.log('Masin temirde deyil!')
+//     }
+// }
+
+// car.marka = 'BMW'
+// car.model = 'Z3';
+// car.speed = 120;
+// console.log(car.drive());
+// console.log(car.temir(true));
+
+
+// var insan = {
+//     adi = "Agshin",
+//     soyadi = "Rajabov",
+//     yas = 28,
+//     adSoyad = function() {
+        
+//     }
+// }
+
+var insan = {
+    adi: "Agshin",
+    soyad: "Rajabov",
+    yas: 28,
+    adSoyad: function() {
+        return this.adi + ' ' + this.soyad;
     }
-    return console.log('Səhifəmizə xoş gəlmisiniz.');
 }
 
-//Massivlər - Array
-var massiv = ['Salam', 17, true, 'Sagol', false, '1990'];
 
-massiv[0] = 'Shuffle';
-massiv[6] = 'University';
+var insan = new Object()
+insan.adi = "Agshin"
+insan.soyad = "Rajabov"
+insan.yas = 28,
+insan.adSoyad = function() {
+    return this.adi + ' ' + this.soyad;
+}
 
-//Salam, 17, 1981
 
-console.log(massiv.length);
+console.log(insan.adSoyad());
+
+console.log(insan.soyad);
+console.log(insan["soyad"]);
